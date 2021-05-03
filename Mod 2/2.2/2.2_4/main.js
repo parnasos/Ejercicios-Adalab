a17155da31a39a7684c43b47c4794cd9b04eaa3c
parnasos <lauragraizer@gmail.com>
2021-05-03 19:52:13 +0200

    Add ex.4 2_2

---
+++ Mod 2/2.2/2.2_4/main.js
@@ -0,0 +1,10 @@
+const totalPrice = 128;
+const amountPeople = 9;
+const extra = 2;
+const priceU = totalPrice / amountPeople;
+console.log(priceU);
+const anaPrice = priceU + extra;
+console.log(anaPrice);
+
+
+
