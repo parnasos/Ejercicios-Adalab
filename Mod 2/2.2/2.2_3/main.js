e6ced36179a369e6622b3caebcb6567911c76bae
parnasos <lauragraizer@gmail.com>
2021-05-03 19:42:43 +0200

    Add ex.3 2_2

---
+++ Mod 2/2.2/2.2_3/main.js
@@ -0,0 +1,13 @@
+const kiwiPrice = 5;
+const pearPrice = 2;
+const grapePrice = 4;
+const qK = 2;
+const qP = 3;
+const qG = 0.5;
+
+const totalPrice = (kiwiPrice*qK + pearPrice*qP + grapePrice*qG);
+
+console.log(totalPrice);
+
+
+
