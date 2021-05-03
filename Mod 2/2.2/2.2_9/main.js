b4fc756995fd30180d0e75831fb9463336b654b6
parnasos <lauragraizer@gmail.com>
2021-05-03 21:41:41 +0200

    Add ex.9 2_2

---
+++ Mod 2/2.2/2.2_9/main.js
@@ -0,0 +1,9 @@
+const nameMate = 'Pamela  Palenciano,';
+
+const phrase= document.querySelector(".text");
+
+phrase.innerHTML= 'El nombre de mi compañera es: ' + nameMate + ' y está compuesto por ' + nameMate.length + ' caracteres.';
+
+
+
+
