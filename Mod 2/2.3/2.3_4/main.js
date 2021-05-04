0f8c410bfda2fec4f759e4b46c60742c541caaf9
parnasos <lauragraizer@gmail.com>
2021-05-04 20:18:52 +0200

    Add ex.4 2.3

---
+++ Mod 2/2.3/2.3_4/main.js
@@ -0,0 +1,13 @@
+'use strict';
+
+let age = 2;
+const dogAge = age*parseInt(5);
+if (age === 1) {
+    console.log('Tu perro tiene 15 años de humano.');
+  } else if (age === 2) {
+    console.log('Tu perro tiene 24 años de humano.');
+  } else  {
+    console.log(`Tu perro tiene ${dogAge} años de humano.`)
+}
+    
+  
