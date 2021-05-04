bf44ea6078bb7404a6da9231f262d44182c9c645
parnasos <lauragraizer@gmail.com>
2021-05-04 19:52:18 +0200

    Add ex.2 2.3

---
+++ Mod 2/2.3/2.3_2/main.js
@@ -0,0 +1,8 @@
+'use strict';
+
+let nombre = "PIli";
+if (nombre === "Laura Graizer"){
+    console.log(`Bienvenida, Laura Graizer`);
+} else {
+    console.log("Lo siento pero el usuario que has introducido no está registrado");
+}
