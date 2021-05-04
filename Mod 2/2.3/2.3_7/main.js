686db15131f8daacc9f9001f10998c8ff7743820
parnasos <lauragraizer@gmail.com>
2021-05-04 20:47:00 +0200

    Add ex.7 2.3

---
+++ Mod 2/2.3/2.3_7/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+let ingredient = 'espinacas';
+
+switch (ingredient) {
+  case 'Pollo':
+    console.log('Te hace un filete con patatas?');
+    break;
+  case 'merluza':
+    console.log('Merluzita en salsa verde es una opción');
+    break;
+  case 'espinacas':
+    console.log('Opta por unas espinacas rehogadas');
+    break;
+  default:
+    console.log('Si no hay nada en la nevera...complicado');
+}
