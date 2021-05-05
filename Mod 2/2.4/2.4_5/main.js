66767b5b032ae6eba2b8152addb1708fff23b008
parnasos <lauragraizer@gmail.com>
2021-05-05 21:00:31 +0200

    Add ex.5 2.4

---
+++ Mod 2/2.4/2.4_5/main.js
@@ -0,0 +1,12 @@
+'use strict';
+
+//let query = document.querySelector(".query")
+
+function getEl (a) {
+return document.querySelector(a);
+
+}
+
+const prueba= getEl(".rueda")
+
+console.log(getEl);
