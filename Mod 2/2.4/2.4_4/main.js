44903462201694792339215008aa30d5584ec5d8
parnasos <lauragraizer@gmail.com>
2021-05-05 18:19:37 +0200

    Add ex.4 2.4

---
+++ Mod 2/2.4/2.4_4/main.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const number = 7;
+const result = number % 2;
+
+
+function parOimpar() {
+  if (result===0) {
+    return true;
+  }
+    return false;
+  };
+
+console.log(parOimpar());
