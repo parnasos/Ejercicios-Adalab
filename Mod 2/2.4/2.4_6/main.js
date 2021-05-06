225a0df49c27b316e1da91d162b1905a294545c3
parnasos <lauragraizer@gmail.com>
2021-05-06 10:59:21 +0200

    Add ex.6 2.4

--- Mod 2/2.4/2.4_6/main.js
+++ Mod 2/2.4/2.4_6/main.js
@@ -4,11 +4,13 @@
 
 function getEl(a) {
     const result =document.querySelector(a);
-    
+    if (result === null) {
+     console.log(`No existe ningún elemento con clase, id o tag llamado ${a}`);
+    }
 return result;
 
 }
 
-const texto = getEl('prueba');
+const texto = getEl('x');
 
 console.log(texto);
