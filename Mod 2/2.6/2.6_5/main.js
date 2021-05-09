56bc6f2d406123d6117bd14e45251f3fb2a7c9ed
parnasos <lauragraizer@gmail.com>
2021-05-09 19:59:07 +0200

    Add ex-5 to 2.6

---
+++ Mod 2/2.6/2.6_5/main.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const text = document.querySelector(".text");
+
+console.dir(text);
+
+//value: está vacío el input
+//node name: tipo de elemento
+//required: si el campo es obligatorio o no. en este caso: false.
