509321d2dac7365fe7fa6f06f336696f938e0cdf
parnasos <lauragraizer@gmail.com>
2021-05-09 20:47:00 +0200

    add ex.7 to 2.6

---
+++ Mod 2/2.6/2.6_7/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const user = {};
+const job = "desarrolladora";
+const age = 37;
+user.firstname = "Ana";
+user.lastname = "Somoza";
+user.age = age+1;
+user.job = job;
+
+console.log(user)
