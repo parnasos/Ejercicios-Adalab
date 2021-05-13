6064437124b7babad693c32ce0cfb7a093675f5a
parnasos <lauragraizer@gmail.com>
2021-05-13 20:46:16 +0200

    Add ex.3 to 2.9

---
+++ Mod 2/2.9/2.9_3/main.js
@@ -0,0 +1,24 @@
+"use strict";
+//const lostNumbers = [4, 8, 15, 16, 23, 42];
+const evenNumbers = [];
+const tripleNumbers = [];
+
+function bestLostNumber() {
+  const lostNumbers = [4, 8, 15, 16, 23, 42];
+  for (let i = 0; i < 6; i++) {
+    if (lostNumbers[i] % 2 === 0) {
+      evenNumbers.push(i);
+    } else if (lostNumbers[i] % 3 === 0) {
+      tripleNumbers.push(i);
+    }
+  }
+  const result = evenNumbers.concat(tripleNumbers);
+  console.log(result);
+  for (let i = 0; i < result.length; i++) {
+    console.log(result[i]);
+  }
+  //console.log(evenNumbers);
+  //console.log(tripleNumbers);
+}
+
+bestLostNumber();
