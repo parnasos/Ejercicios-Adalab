6c5403bfa89f9e46342052f73dcf635fd0ce73c0
parnasos <lauragraizer@gmail.com>
2021-05-19 17:56:12 +0200

    Add ex.9 to 2.13

---
+++ Mod 2/2.13/2.13_9/main.js
@@ -0,0 +1,24 @@
+"use strict";
+
+const runners = [
+  { name: 'Gregory Goyle', time: 56, student: true },
+  { name: 'Nymphadora Tonks', time: 9, student: false },
+  { name: 'Luna Lovegood', time: 45, student: true },
+  { name: 'Cedric Diggory', time: 28, student: true },
+  { name: 'Cho Chang', time: 35, student: true }
+];
+
+const winner = 
+
+runners.filter((runner)=> (runner.student === true)).reduce((acc, runner) => {
+ if (acc.time > runner.time) {
+  return (acc = runner);
+} else {
+  return acc;
+  }
+});
+
+
+console.log(
+  `La ganadora es ${winner.name} con un tiempo record de: ${winner.time} minutos`
+);
