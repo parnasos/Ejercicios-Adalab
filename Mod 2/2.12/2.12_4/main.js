052dd1cf626871e06d26e9efaaf4633bb76e284e
parnasos <lauragraizer@gmail.com>
2021-05-19 11:11:55 +0200

    Update ex.4 2.12

--- Mod 2/2.12/2.12_4/main.js
+++ Mod 2/2.12/2.12_4/main.js
@@ -1,8 +1,15 @@
 "use strict";
 const pText = document.querySelector(".js-p");
+const container = document.querySelector(".js-container");
 
 console.log(pText.offsetHeight);
 const ptextHeight = pText.offsetHeight;
 
 pText.style.offsetHeight = (1 / 3) * pText.offsetHeight;
-console.log(pText.style.offsetHeight);
+console.log(`Un tercio del <p> son: ${pText.style.offsetHeight} px`);
+
+const containerheight = container.offsetHeight;
+container.style.offsetHeight = (1 / 3) * container.offsetHeight;
+console.log(
+  `Un tercio del "container" son: ${container.style.offsetHeight} px`
+);
