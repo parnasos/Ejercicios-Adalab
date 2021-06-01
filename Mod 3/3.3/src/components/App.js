83178fcd1f1d411106297351189b8058afd229fc
parnasos <lauragraizer@gmail.com>
2021-06-01 17:45:22 +0200

    Add ex. 1 to 3.3

---
+++ Mod 3/3.3/src/components/App.js
@@ -0,0 +1,60 @@
+import React from "react";
+import "./App.css";
+
+class App extends React.Component {
+  render() {
+    const students = [
+      {
+        promo: "A",
+        name: "Sofía",
+        age: 20,
+      },
+      {
+        promo: "B",
+        name: "María",
+        age: 21,
+      },
+      {
+        promo: "A",
+        name: "Lucía",
+        age: 22,
+      },
+    ];
+
+    //LISTADO DE PROMOS SIN FILTRAR
+    /*return (
+      <div>
+        <h1>Pintar listados de nombres y edades:</h1>
+
+        {students.map((student) => {
+          return (
+            <li>
+              <p>Promo: {student.promo}</p>
+              <p>Nombre: {student.name}</p>
+              <p>Edad: {student.age}</p>
+            </li>
+          );
+        })}
+      </div>
+    )*/
+    // LISTADO FILTRANDO LA PROMO A
+    return (
+      <div>
+        ;<h1>Pintar listados de nombres y edades:</h1>
+        {students
+          .filter((student) => student.promo === "A")
+          .map((student) => {
+            return (
+              <li>
+                <p>Promo: {student.promo}</p>
+                <p>Nombre: {student.name}</p>
+                <p>Edad: {student.age}</p>
+              </li>
+            );
+          })}
+      </div>
+    );
+  }
+}
+
+export default App;
