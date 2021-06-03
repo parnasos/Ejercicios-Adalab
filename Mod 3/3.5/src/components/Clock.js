79046fa3c43f7ee9956e6175adf37c93cb4f6ba2
parnasos <lauragraizer@gmail.com>
2021-06-03 20:40:32 +0200

    Update ex.3

--- Mod 3/3.5/src/components/Clock.js
+++ Mod 3/3.5/src/components/Clock.js
@@ -4,6 +4,7 @@ import React from "react";
 class Clock extends React.Component {
   constructor(props) {
     super(props);
+
     this.state = { date: new Date() };
     this.updateClock = this.updateClock.bind(this);
     setInterval(() => {
