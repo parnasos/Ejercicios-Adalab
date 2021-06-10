5eab14ba7f496134bd6ee38f8ec618ddb700d11a
parnasos <lauragraizer@gmail.com>
2021-06-11 00:09:35 +0200

    Update ex.1 3.8

--- Mod 3/3.8/src/components/App.js
+++ Mod 3/3.8/src/components/App.js
@@ -1,9 +1,80 @@
 import React from "react";
-import "../stylesheets/App.css";
-import RandomUser from "./RandomUser";
+import getDataFromAPI from "../services/getDataFromAPI";
 
-const App = () => {
-  return <RandomUser />;
-};
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      profiles: [],
+      filterCity: "",
+      filterGender: "",
+    };
+    getDataFromAPI().then((data) => {
+      console.log(data);
+      this.setState({
+        profiles: data,
+      });
+    });
+
+    this.handleFilterCity = this.handleFilterCity.bind(this);
+    this.handleFilterGender = this.handleFilterGender.bind(this);
+  }
+  handleFilterCity(ev) {
+    this.setState({
+      filterCity: ev.target.value,
+    });
+  }
+
+  handleFilterGender(ev) {
+    this.setState({
+      filterGender: ev.target.value,
+    });
+  }
+
+  renderProfiles() {
+    return this.state.profiles
+      .filter((profile) => {
+        return profile.city
+          .toLowerCase()
+          .includes(this.state.filterCity.toLowerCase());
+      })
+      .filter((profile) => {
+        return profile.gender
+          .toLowerCase()
+          .includes(this.state.filterGender.toLowerCase());
+      })
+      .map((profile) => {
+        return (
+          <li key={profile.id}>
+            <h3>Name:{profile.name}</h3>
+            <p>City:{profile.city}</p>
+            <p>Age:{profile.age}</p>
+            <p>Gender:{profile.gender}</p>
+            <img src={profile.image}></img>
+          </li>
+        );
+      });
+  }
+
+  render() {
+    return (
+      <div>
+        <p>Random Profiles</p>
+        <input
+          type="text"
+          placeholder="Search according to the city"
+          onChange={this.handleFilterCity}
+        />
+        <input
+          type="text"
+          placeholder="Search according to the gender"
+          onChange={this.handleFilterGender}
+        />
+        <ul>{this.renderProfiles()}</ul>
+      </div>
+    );
+  }
+}
 
 export default App;
--- Mod 3/3.8/src/components/fetchRandom.js
+++
@@ -1,7 +0,0 @@
-const dataRandom = "https://randomuser.me/api/?results=50";
-
-const fetchRandom = () => {
-  return fetch(dataRandom).then((response) => response.json()); // Devuelve la Promise que genera el fetch
-};
-
-export { fetchRandom };
