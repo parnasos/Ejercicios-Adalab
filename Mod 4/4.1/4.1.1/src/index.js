282ac6e2ca6ff43a2edd3861aba802eb97a7a6ce
parnasos <lauragraizer@gmail.com>
2021-06-21 20:25:52 +0200

    Add 1,2 and 3 of 4.1

--- Mod 4/4.1/4.1.1/src/index.js
+++ Mod 4/4.1/4.1.1/src/index.js
@@ -1,29 +1,7 @@
 const fs = require("fs");
 
-const readFile = (fileName, callback) => {
-  fs.readFile(fileName, "utf8", (err, data) => {
-    if (err) {
-      console.log("Error:", err);
-    } else {
-      callback(data);
-    }
-  });
+const sum = (a, b) => {
+  return a + b;
 };
 
-const writeFile = (fileName, fileContent, callback) => {
-  fs.writeFile(fileName, fileContent, (err) => {
-    if (err) {
-      console.log("Error:", err);
-    } else {
-      callback();
-    }
-  });
-};
-
-readFile("./input.txt", (fileContent) => {
-  const currentDate = new Date().toString();
-  const newFileContent = `${currentDate}: ${fileContent}`;
-  writeFile("./output.txt", newFileContent, () => {
-    console.log("The file has been copied!");
-  });
-});
+console.log(sum(3, 7));
